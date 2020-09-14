6dfa31a648924a8506ea6a678fdbd4306ea6b619
Mounikaran <48761585+Mounikaran@users.noreply.github.com>
2020-09-14 12:04:17 +0530

    Auto Profile Table Creation

--- codeq/codeq/settings.py
+++ codeq/codeq/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
 
     'useraccount',
     'post',
+    'tags',
 ]
 
 SITE_ID = 1
--- codeq/useraccount/models.py
+++ codeq/useraccount/models.py
@@ -1,11 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+
+
+@receiver(post_save, sender=User)
+def create_favorites(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_pic = models.ImageField(upload_to='profile_pics', default="default_img.png")
+    tags = models.ManyToManyField(
+        'tags.Tag', related_name='user_tags', blank=True)
 
     def __str__(self):
         return self.user.username
